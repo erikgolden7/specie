3f26c891cd3d312b3eeebf8e8a76df1d052cdb02
Erik Golden <erikgolden7@gmail.com>
2018-09-02 14:12:03 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="nav">
+          <div className="logo" />
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
