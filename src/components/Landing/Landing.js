30e4dbd18c495ed51b3624af0cca4544e660af59
Erik Golden <erikgolden7@gmail.com>
2018-09-13 17:26:40 -0500

    about section -- landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './landing.css';
 import img from '../../assets/money-management.jpg';
+import icon1 from '../../assets/budgets.png';
+import icon2 from '../../assets/bills_calendar.png';
+import icon3 from '../../assets/credit_checked.png';
 
 export default class Landing extends Component {
   render() {
@@ -14,6 +17,13 @@ export default class Landing extends Component {
               Easily create budgets, and see our suggestions based on your
               spending.
             </p>
+            <img
+              className=""
+              src={icon1}
+              alt="broken"
+              width="90"
+              height="auto"
+            />
           </div>
           <div className="feature-box">
             <h3>Bills? Done</h3>
@@ -21,6 +31,13 @@ export default class Landing extends Component {
               Track your bills like never before. Get alerts when it's time to
               pay so you never miss one.
             </p>
+            <img
+              className=""
+              src={icon2}
+              alt="broken"
+              width="60"
+              height="auto"
+            />
           </div>
           <div className="feature-box">
             <h3>Credit score? Checked</h3>
@@ -28,7 +45,19 @@ export default class Landing extends Component {
               Find out yours for free and get tips to help improve it, no credit
               card required.
             </p>
+            <img
+              className=""
+              src={icon3}
+              alt="broken"
+              width="90"
+              height="auto"
+            />
           </div>
+        </section>
+        <section className="about">
+          <div className="about-section">top</div>
+          <div className="about-section">middle</div>
+          <div className="about-section">bottom</div>
         </section>
       </div>
     );
