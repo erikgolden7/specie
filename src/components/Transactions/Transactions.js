2f6c89c37e12551723849a6de91e4d24bb8b72db
Erik Golden <erikgolden7@gmail.com>
2018-09-26 21:17:41 -0500

    added table to transactions

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -5,14 +5,42 @@ import TransactionModal from './TransactionModal';
 
 import './transactions.css';
 
+const testData = [
+  { type: 'Shopping', location: 'Walmart', date: '4/30/2018', amount: 200 },
+  { type: 'Home Improvement', location: 'Home Depot', date: '6/13/2018', amount: 300 },
+  { type: 'Food', location: 'Chilis', date: '4/23/2018', amount: 600 },
+  { type: 'Clothes', location: 'Mr Mac', date: '12/22/2018', amount: 800 },
+  { type: 'Shopping', location: 'RC Willey', date: '7/04/2018', amount: 100 }
+];
+
 class Transactions extends Component {
   render() {
+    const dataMap = testData.map(e => {
+      return (
+        <tr>
+          <td>{e.type}</td>
+          <td>{e.location}</td>
+          <td>{e.date}</td>
+          <td>{e.amount}</td>
+        </tr>
+      );
+    });
+
     const { showModal, flagToggle } = this.props;
     return (
       <div style={{ marginTop: 60 }}>
         <div className="add-transaction-container">
           <button onClick={flagToggle}>Add New Transaction</button>
-          {showModal ? <TransactionModal /> : ''}
+          {showModal && <TransactionModal />}
+        </div>
+        <div className="display-transactions">
+          <th>
+            <td>Type</td>
+            <td>Location</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </th>
+          {dataMap}
         </div>
       </div>
     );
