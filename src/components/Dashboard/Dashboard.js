a1f4e4f2129964ae3517b319d8d2ca1207e998d3
Erik Golden <erikgolden7@gmail.com>
2018-10-05 12:03:09 -0500

    pie chart with test data

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 // import { BarChart } from 'recharts';
-import { BarChart, Bar, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  PieChart,
+  Pie,
+  Cell
+} from 'recharts';
 
 class Dashboard extends Component {
   render() {
@@ -14,6 +26,14 @@ class Dashboard extends Component {
       { name: 'July', income: 3490, spent: -4300, amt: 2100 }
     ];
 
+    const data1 = [
+      { name: 'Group A', value: 400 },
+      { name: 'Group B', value: 300 },
+      { name: 'Group C', value: 300 },
+      { name: 'Group D', value: 200 }
+    ];
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
     return (
       <div style={{ marginTop: 80 }}>
         <BarChart width={600} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
@@ -26,6 +46,14 @@ class Dashboard extends Component {
           <Bar dataKey="spent" fill="#EB615F" />
           <Bar dataKey="income" fill="#4EC375" />
         </BarChart>
+
+        <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+          <Pie data={data1} cx={300} cy={200} label outerRadius={80} fill="#8884d8">
+            {data1.map((e, i) => (
+              <Cell fill={colors[i % colors.length]} />
+            ))}
+          </Pie>
+        </PieChart>
       </div>
     );
   }
