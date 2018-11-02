38973d86a54789203e346f6f25a4efa3f2e53140
Erik Golden <erikgolden7@gmail.com>
2018-11-01 22:08:10 -0600

    export csv

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -74,7 +74,6 @@ class Overview extends Component {
         }
       }
     });
-    console.log(chartData);
 
     return chartData;
   };
@@ -110,6 +109,57 @@ class Overview extends Component {
         this.setState({ pieMonth: this.state.pieMonth - 1 });
       }
     }
+  };
+
+  convertArrayOfObjectsToCSV = args => {
+    var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+    data = args.data || null;
+    if (data == null || !data.length) {
+      return null;
+    }
+
+    columnDelimiter = args.columnDelimiter || ',';
+    lineDelimiter = args.lineDelimiter || '\n';
+
+    keys = Object.keys(data[0]);
+
+    result = '';
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+
+    data.forEach(item => {
+      ctr = 0;
+      keys.forEach(key => {
+        if (ctr > 0) result += columnDelimiter;
+
+        result += item[key];
+        ctr++;
+      });
+      result += lineDelimiter;
+    });
+
+    return result;
+  };
+
+  downloadCSV = (args, pieData) => {
+    var data, filename, link;
+    var csv = this.convertArrayOfObjectsToCSV({
+      data: pieData
+    });
+    if (csv == null) return;
+
+    filename = args.filename || 'export.csv';
+
+    if (!csv.match(/^data:text\/csv/i)) {
+      csv = 'data:text/csv;charset=utf-8,' + csv;
+    }
+    data = encodeURI(csv);
+
+    link = document.createElement('a');
+    link.setAttribute('href', data);
+    link.setAttribute('download', filename);
+    link.click();
   };
 
   render() {
@@ -185,6 +235,7 @@ class Overview extends Component {
               <h4>{`$${total.toFixed(2)}`}</h4>
             </div>
           </div>
+          <p onClick={() => this.downloadCSV({ filename: 'budget-data.csv' }, pieData)}>Download CSV</p>
         </div>
       </div>
     );
