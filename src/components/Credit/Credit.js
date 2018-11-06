6e30031c7630f2ac1417be2bcd71c08034a469fa
Erik Golden <erikgolden7@gmail.com>
2018-11-06 17:52:05 -0600

    clean up credit arc code

--- src/components/Credit/Credit.js
+++ src/components/Credit/Credit.js
@@ -6,7 +6,7 @@ class Credit extends Component {
     return (
       <div>
         Credit
-        <ul className="chart-skills">
+        <ul className="chart-credit">
           <li>
             <span>300</span>
           </li>
@@ -32,15 +32,3 @@ class Credit extends Component {
 }
 
 export default Credit;
-
-// style="fill: rgb(245, 70, 98); transition-delay: 300ms; transition-duration: 966.667ms;"
-
-// style="fill: rgb(253, 184, 70); transition-delay: 966.667ms; transition-duration: 966.667ms;"
-
-// style="fill: rgb(110, 224, 108); transition-delay: 1266.67ms; transition-duration: 966.667ms;"
-
-// style="fill: rgb(10, 199, 117); transition-delay: 1566.67ms; transition-duration: 300ms;"
-
-// style="fill: rgb(2, 159, 125); transition-delay: 1866.67ms; transition-duration: 300ms;"
-
-// style="transform: rotate(60.52deg) translateX(162px) rotate(-270deg); fill: rgb(110, 224, 108);"
